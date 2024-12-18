5d16d745987d4ec881b00bd32a54bb99ae36f3da
Ratmir Mugattarov <ratmir_mugattarov@bitbucket.com>
2024-12-18 13:28:51 +1100

    initial commit

--- setup_dfu.bat
+++ setup_dfu.bat
@@ -17,39 +17,30 @@ if %errorLevel% neq 0 (
 echo [1/5] Downloading required files...
 :: Download Python installer
 curl -L -o python_installer.exe https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe
-:: Download Zadig
-curl -L -o zadig.exe https://github.com/pbatard/libwdi/releases/download/v1.5.0/zadig-2.8.exe
 
 echo [2/5] Installing Python...
 python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-if %ERRORLEVEL% neq 0 (
-    echo Python installation failed
-    pause
-    exit /b 1
-)
+:: Wait for installation
+timeout /t 10
 
 echo [3/5] Setting up Python environment...
-:: Add Python to current session PATH
-set PATH=%PATH%;C:\Program Files\Python310;C:\Program Files\Python310\Scripts
+:: Create new virtual environment in Windows path
+C:\Program Files\Python310\python.exe -m venv C:\dfu_venv
+call C:\dfu_venv\Scripts\activate.bat
 
-:: Install required Python packages
-python -m pip install --upgrade pip
-python -m pip install pyusb pypiwin32 wheel libusb-package libusb libusb1 pyusb-backend-libusb1
+:: Install required packages
+C:\dfu_venv\Scripts\python.exe -m pip install --upgrade pip
+C:\dfu_venv\Scripts\pip install pyusb
 
-echo [4/5] Installing Zadig...
-start /wait zadig.exe
-
-echo [5/5] Cleanup...
+echo [4/5] Cleanup...
 del python_installer.exe
-del zadig.exe
 
 echo =====================================
 echo Installation completed!
 echo.
 echo Next steps:
-echo 1. Put your device in DFU mode
-echo 2. Run Zadig and install WinUSB driver
-echo 3. Use update_firmware.bat to flash your device
+echo 1. Make sure device is in DFU mode
+echo 2. Verify Zadig installed WinUSB driver
 echo =====================================
 
 pause
--- update_firmware.bat
+++ update_firmware.bat
@@ -5,12 +5,17 @@ echo =====================================
 echo DFU Firmware Update Tool
 echo =====================================
 
+:: Activate the virtual environment
+call C:\dfu_venv\Scripts\activate.bat
+
 echo [1/2] Checking device connection...
-python dfu.py -l
+C:\dfu_venv\Scripts\python.exe dfu.py -l
 
 echo [2/2] Running firmware update...
-python dfu.py -m
-python dfu.py "rbms-pybdsf2w-usg-ribv3.dfu"
+C:\dfu_venv\Scripts\python.exe dfu.py -m
+C:\dfu_venv\Scripts\python.exe dfu.py "rbms-pybdsf2w-usg-ribv3.dfu"
+
+deactivate
 
 echo =====================================
 echo Firmware update completed
